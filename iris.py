c366f671107a51b5d384de3c13a4f65c1e311453
Andreas Mautsch <amautsch>
2022-07-10 09:32:42 +0200

    initial import

--- iris.py
+++ iris.py
@@ -1,25 +1,33 @@
-import warnings
-warnings.filterwarnings('ignore')
-
-import matplotlib.pylab as plt
-import numpy as np
-
-import sklearn
-print(sklearn.__version__)
-
-from sklearn.datasets import load_iris
-iris = load_iris()
-
-print(iris.DESCR)
-
+# load iris data and load 0,1,2,4 from matrix/map
+from sklearn import datasets
+iris = datasets.load_iris()
+#this is the input data as a complex matrix with sepal + petal, width/length
 X = iris.data
+#this is the classification data which can be 0,1,2 base on the 3 possible types
 y = iris.target
 
-print(X)
-print(y)
+#print(iris.DESCR)
 
+X_sepal_length = X[:, 0]
+X_sepal_width = X[:, 1]
+X_petal_length = X[:, 2]
+X_petal_width = X[:, 3]
 
-X.shape, y.shape
-X[0]
+# split iris data into 60% training and 40% test data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
-print(X[0])
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+# nearest neighbour estimator
+from sklearn import neighbors
+clf = neighbors.KNeighborsClassifier(1)
+
+#train the model
+clf.fit(X_train, y_train)
+
+#predict a concrete example
+p = clf.predict([[6.3, 2.7, 5.5, 1.5]])
+print(p)
+
+z = 5
