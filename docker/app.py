07d7bc01e2e6a0b928e340959cc621e9f1d4c745
Andreas Mautsch <amautsch>
2022-09-19 15:25:20 +0200

    file processing added

--- docker/app.py
+++ docker/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
+import socket
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+@app.route("/")
+def hello():
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
-
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
