539633a897e410affee38d1a7958874e42b7228b
Andreas Mautsch <amautsch>
2022-09-20 15:20:11 +0200

    changes

--- body_size.py
+++ body_size.py
@@ -4,22 +4,24 @@ from sklearn import neighbors
 
 
 def read_file():
-    return panda.read_csv('doc/body_size.csv')
+    return panda.read_csv('doc/body_size_short.csv')
+
+
+def train_me():
+    X = df.values[:, 0:1]
+    y = df.values[:, 1:2]
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+    clf = neighbors.KNeighborsClassifier(1)
+    clf.fit(x_train, y_train.ravel())
+
+    print("normal train score", clf.score(x_train, y_train))
+    print("normal test score", clf.score(x_test, y_test))
+    return clf
 
 
 df = read_file()
+clfn = train_me()
+p = clfn.predict([[54]])
 
-X = df.values[:, 0:1]
-y = df.values[:, 1:2]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
-
-clf = neighbors.KNeighborsClassifier(1)
-clf.fit(X_train, y_train.ravel())
-
-p = clf.predict([[180]])
-
-print("normal train score", clf.score(X_train, y_train))
-print("normal test score", clf.score(X_test, y_test))
-print("")
 print("preedict", p)
 
