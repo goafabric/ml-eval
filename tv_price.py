021c3b3c9d45b94fd00a4fa1a7361818bb6b819d
Andreas Mautsch <amautsch>
2022-09-20 16:54:32 +0200

    changes

---
+++ tv_price.py
@@ -0,0 +1,27 @@
+import pandas as panda
+from sklearn.model_selection import train_test_split
+from sklearn import neighbors
+
+
+def read_file():
+    return panda.read_csv('doc/tv_price.csv')
+
+
+def train_me():
+    values = df.values[:, 0:1]
+    target = df.values[:, 1:2]
+    values_train, values_test, target_train, target_test = train_test_split(values, target, test_size=0.4)
+    clf = neighbors.KNeighborsClassifier(1)
+    clf.fit(values_train, target_train.ravel())
+
+    print("normal train score", clf.score(values_train, target_train))
+    print("normal test score", clf.score(values_test, target_test))
+    return clf
+
+
+df = read_file()
+clfn = train_me()
+p = clfn.predict([[36]])
+
+print("preedict", p)
+
