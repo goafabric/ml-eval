5fbbd25a2f6a721209873458b436e1af0b5ba95f
Andreas Mautsch <amautsch>
2022-09-20 11:16:27 +0200

    changes

--- file_process.py
+++ file_process.py
@@ -7,18 +7,20 @@ def read_file():
                        names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class'])
 
 
+def search_missing_sepal():
+    print('looking for missing sepal')
+    print(df.count())
+    print(df[df['sepal width'].isnull()])
+
+
 def calculate_missing_sepal():
+    print('correcting missing')
     iris_versicolor = df[df['class'] == 'Iris-versicolor']
     mean_sepal_width = pd.Series.mean(iris_versicolor['sepal width'])
     df.loc[82, 'sepal width'] = mean_sepal_width
+    print(df.count())
 
 
 df = read_file()
-
-print('looking for missing sepal')
-print(df.count())
-print(df[df['sepal width'].isnull()])
-
-print('correcting missing')
+search_missing_sepal()
 calculate_missing_sepal()
-print(df.count())
