4f282924cc101507702c485cdb9eee96b4d70a62
Andreas Mautsch <amautsch>
2022-09-20 09:47:40 +0200

    file processing added

--- iris.py
+++ iris.py
@@ -1,5 +1,7 @@
 # load iris data and load 0,1,2,4 from matrix/map
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn import neighbors
 iris = datasets.load_iris()
 # this is the input data as a complex matrix with sepal + petal, width/length
 X = iris.data
@@ -7,17 +9,18 @@ X = iris.data
 y = iris.target
 
 # split iris data into 60% training and 40% test data
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
-print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
-# nearest neighbour estimator
-from sklearn import neighbors
-clf = neighbors.KNeighborsClassifier(1)
+def train_normal():
+    # nearest neighbour estimator
+    clf = neighbors.KNeighborsClassifier(1)
+    # train the model
+    clf.fit(X_train, y_train)
+    return clf
 
-# train the model
-clf.fit(X_train, y_train)
+
+clf = train_normal()
 
 
 def predict_concrete_example():
